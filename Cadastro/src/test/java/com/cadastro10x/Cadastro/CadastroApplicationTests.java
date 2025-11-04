3c20332a7cd834311c8a775e8dadb7290852f46f
j-adenilson <j.adenilson@outlook.com.br>
2025-11-03 23:55:53 -0300

    Add arquivos e dependencias do projeto

---
+++ Cadastro/src/test/java/com/cadastro10x/Cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro10x.Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
