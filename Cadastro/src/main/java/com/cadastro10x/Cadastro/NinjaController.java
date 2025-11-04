e8d316f62f4a06f4f2640eba631c14b8b3bec482
j-adenilson <j.adenilson@outlook.com.br>
2025-11-04 11:21:35 -0300

    Entidades e persistência de dados - Tabela de Ninjas

--- Cadastro/src/main/java/com/cadastro10x/Cadastro/NinjaController.java
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/NinjaController.java
@@ -1,13 +1,12 @@
 package com.cadastro10x.Cadastro;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasvindas(){
