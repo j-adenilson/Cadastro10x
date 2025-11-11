466492f314536bf2d6f9a1a63ef47db1384a2e69
j-adenilson <j.adenilson@outlook.com.br>
2025-11-10 23:44:24 -0300

    criando interface MissoesRepository

---
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/Missoes/MissoesRepository.java
@@ -0,0 +1,7 @@
+package com.cadastro10x.Cadastro.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+
+}
---
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/Ninjas/NinjaService.java
@@ -0,0 +1,4 @@
+package com.cadastro10x.Cadastro.Ninjas;
+
+public class NinjaService {
+}
