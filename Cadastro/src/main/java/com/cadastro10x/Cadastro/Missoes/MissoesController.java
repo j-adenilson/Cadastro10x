1a5f4b331960a4966cf914a03b7722c6523e26f7
j-adenilson <j.adenilson@outlook.com.br>
2025-11-14 13:29:02 -0300

    feat: cria as rotas do  Missoes Controller com mock

---
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/Missoes/MissoesController.java
@@ -0,0 +1,28 @@
+package com.cadastro10x.Cadastro.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
+}
