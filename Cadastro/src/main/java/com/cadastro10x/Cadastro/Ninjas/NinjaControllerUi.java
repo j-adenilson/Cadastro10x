2bd393b4a76670b917502f7438c0182687a47112
j-adenilson <j.adenilson@outlook.com.br>
2025-11-20 20:43:03 -0300

    introdução ao thymeLeaf - Parte 2

---
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/Ninjas/NinjaControllerUi.java
@@ -0,0 +1,28 @@
+package com.cadastro10x.Cadastro.Ninjas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    //localhost:8080/ninjas/ui/listar
+    @GetMapping("/listar")
+    public String listarNinjas(Model model){
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "listarNinjas";
+    }
+}
