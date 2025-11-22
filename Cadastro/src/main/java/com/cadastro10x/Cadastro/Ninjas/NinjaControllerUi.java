a19ac2c24e458e59017ca354cbd288ce107b7a71
j-adenilson <j.adenilson@outlook.com.br>
2025-11-21 23:52:26 -0300

    thymeLeaf - Parte 2

--- Cadastro/src/main/java/com/cadastro10x/Cadastro/Ninjas/NinjaControllerUi.java
+++ Cadastro/src/main/java/com/cadastro10x/Cadastro/Ninjas/NinjaControllerUi.java
@@ -1,10 +1,11 @@
 package com.cadastro10x.Cadastro.Ninjas;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,5 +25,30 @@ public class NinjaControllerUi {
         List<NinjaDTO> ninjas = ninjaService.listarNinjas();
         model.addAttribute("ninjas", ninjas);
         return "listarNinjas";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarNinjaPorId(@PathVariable Long id){
+        ninjaService.deletarNinjaPorId(id);
+        return "redirect:/ninjas/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarNinjasPorId(@PathVariable Long id, Model model){
+        NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
+        if (ninja != null){
+            model.addAttribute("ninjas", ninja);
+            return "detalhesninja";
+        }
+        else {
+            model.addAttribute("mensagem", "Ninja Não encontrado");
+            return "listarNinjas";
+        }
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("mensagem", "Ninja salvo com sucesso");
+        return "redirect/ninjas/ui/listra";
     }
 }
